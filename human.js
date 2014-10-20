3e629658563d7a45772084d4f95f5a5da5a78775
Andrew Sinner <andrewsinner@gmail.com>
2014-10-20 16:55:30 -0700

    Syntax

--- human.js
+++ human.js
@@ -5,12 +5,12 @@ function Human(name, job, salary) {
 }
 Human.prototype.greet = function() {
 	return "Hello! My name is " + this.name;
-}
+};
 Human.prototype.boast = function() {
 	return "My job? Oh I'm a " + job + "and I make $" + salary;
-}
+};
 Human.prototype.toString = function() {
 	for (var attribute in this) {
 		console.log(attribute + " = " + this[attribute]);
-	}
-}
+	};
+};
