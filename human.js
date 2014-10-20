25b4db2b8ccab44c12fcc3609271b1fcee8ccc4c
Andrew Sinner <andrewsinner@gmail.com>
2014-10-20 16:43:14 -0700

    Add human object

---
+++ human.js
@@ -0,0 +1,16 @@
+function Human(name, job, salary) {
+	this.name = name;
+	this.job = job;
+	this.salary = salary;
+}
+Human.prototype.greet = function() {
+	return "Hello! My name is " + this.name;
+}
+Human.prototype.boast = function() {
+	return "My job? Oh I'm a " + job + "and I make $" + salary;
+}
+Human.prototype.toString = function() {
+	for (var attribute in this) {
+		console.log(attribute + " = " + this[attribute]);
+	}
+}
